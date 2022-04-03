8fba31a6fe7284916f623ed95bb34b5a1fde900f
Abhishek <abhijain2196@gmail.com>
2022-04-03 13:16:28 +0530

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NFT DROP</title>
+        <link
+          rel="icon"
+          href="https://cdn-icons-png.flaticon.com/128/6298/6298900.png"
+        />
+      </Head>
+      <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </>
+  )
 }
 
 export default MyApp
